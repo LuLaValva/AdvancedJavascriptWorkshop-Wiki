b7f1c302003a2e24481eb7b0421d2fb2698002f7
LuLaValva <lulavalva@gmail.com>
2020-12-07 17:44:18 -0500

    Extract function

--- views/js/standard_page_client.js
+++ views/js/standard_page_client.js
@@ -5,29 +5,35 @@ function toggleModuleEditMode(element) {
 
   if (container.classList.toggle("editmode")) {
     element.innerHTML = "(submit)";
+
     setEditMode(title, true);
     setEditMode(content, true);
   } else {
     element.innerHTML = "(edit)";
+
     setEditMode(title, false);
     setEditMode(content, false);
 
     moduleIndex = +container.id.substring(2);
 
-    fetch(`${window.location.href}/update_module`, {
-      method: "POST",
-      headers: new Headers({
-        "Content-Type": "application/json;charset=utf-8",
-      }),
-      body: JSON.stringify({
-        index: moduleIndex,
-        name: title.innerHTML,
-        content: content.innerHTML,
-      }),
-    }).then((response) => {
-      console.log(response);
-    });
+    updateModuleWithHTTP(moduleIndex, title.innerHTML, content.innerHTML);
   }
+}
+
+function updateModuleWithHTTP(moduleIndex, updatedName, updatedContent) {
+  fetch(`${window.location.href}/update_module`, {
+    method: "POST",
+    headers: new Headers({
+      "Content-Type": "application/json;charset=utf-8",
+    }),
+    body: JSON.stringify({
+      index: moduleIndex,
+      name: updatedName,
+      content: updatedContent,
+    }),
+  }).then((response) => {
+    console.log(response);
+  });
 }
 
 function setEditMode(element, canEdit) {
